7e7dfb0819aa8e232e074933f2756289909c095e
Rocangus <hunter_sniper112@hotmail.com>
2019-11-06 11:38:30 +0100

    Initial setup

---
+++ Kattis-HtoO/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kattis_HtoO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string outputMolecule = Console.ReadLine();
+            var inputStrings = input.Split();
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+        }
+    }
+}
