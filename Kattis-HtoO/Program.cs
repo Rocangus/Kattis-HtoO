962d60573c22573361a605ace20c4b5c7ebba57b
Rocangus <hunter_sniper112@hotmail.com>
2019-11-07 09:41:16 +0100

    Refactoring for unit testing

--- Kattis-HtoO/Program.cs
+++ Kattis-HtoO/Program.cs
@@ -3,44 +3,77 @@ using System.Collections.Generic;
 
 namespace Kattis_HtoO
 {
-    class Program
+    public class Program
     {
+        static bool inputIsSource;
         static void Main(string[] args)
         {
-            Dictionary<char, int> sourceMolecules = new Dictionary<char, int>();
+            Dictionary<char, int> sourceAtoms = new Dictionary<char, int>();
             Dictionary<char, int> productMolecule = new Dictionary<char, int>();
             string input = Console.ReadLine();
-            string outputMolecule = Console.ReadLine();
+            inputIsSource = true;
             var inputStrings = input.Split();
-            var inputMolecule = inputStrings[0];
-            for (int i = 0; i < inputMolecule.Length; i++)
+            sourceAtoms = ParseInput(sourceAtoms, inputStrings);
+            string outputMolecule = Console.ReadLine();
+        }
+
+        public static Dictionary<char, int> ParseInput(Dictionary<char, int> atoms, string[] inputStringArray)
+        {
+            var inputSource = inputStringArray[0];
+            for (int i = 0; i < inputSource.Length; i++)
             {
-                var c = inputMolecule[i];
+                var c = inputSource[i];
                 if (!char.IsDigit(c))
                 {
                     List<int> ints = new List<int>();
-                    int x = i + 1;
-                    int value;
-                    while(x < inputMolecule.Length && char.IsDigit(inputMolecule[x]))
-                    {
-                        ints.Add((int)char.GetNumericValue(inputMolecule[x]));
-                        x++;
-                    }
-                    if (ints.Count > 0)
-                    {
-                        value = int.Parse(String.Concat(ints));
-                        i += ints.Count;
-                    }
-                    else { value = 1; }
-                    if (sourceMolecules.ContainsKey(c)) 
-                    {
-                        sourceMolecules[c] = value + sourceMolecules[c];
-                    }
-                    else
-                    {
-                        sourceMolecules.Add(c, value);
-                    }
-                } 
+                    int value = DetermineAtomCount(inputSource, ref i, ints);
+                    UpdateAtomCounts(atoms, c, value);
+                }
+            }
+            if (inputIsSource) MultiplySourceMolecule(atoms, inputStringArray);
+            return atoms;
+        }
+
+        public static int DetermineAtomCount(string inputSource, ref int i, List<int> ints)
+        {
+            int x = i + 1;
+            int value;
+            while (x < inputSource.Length && char.IsDigit(inputSource[x]))
+            {
+                ints.Add((int)char.GetNumericValue(inputSource[x]));
+                x++;
+            }
+            if (ints.Count > 0)
+            {
+                value = int.Parse(String.Concat(ints));
+                i += ints.Count;
+            }
+            else { value = 1; }
+
+            return value;
+        }
+
+        public static void UpdateAtomCounts(Dictionary<char, int> sourceAtoms, char c, int value)
+        {
+            if (sourceAtoms.ContainsKey(c))
+            {
+                sourceAtoms[c] += value;
+            }
+            else
+            {
+                sourceAtoms.Add(c, value);
+            }
+        }
+
+        public static void MultiplySourceMolecule(Dictionary<char, int> sourceAtoms, string[] inputStrings)
+        {
+            if (inputStrings.Length == 2)
+            {
+                var countMultiplier = int.Parse(inputStrings[1]);
+                foreach (var key in sourceAtoms.Keys)
+                {
+                    sourceAtoms[key] *= countMultiplier;
+                }
             }
         }
     }
